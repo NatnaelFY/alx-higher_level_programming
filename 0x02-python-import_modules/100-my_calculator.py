17e254fef6dd51266cc14f5bebbddc04b48ec85f
NatnaelFY <natfiky2020@gmail.com>
2022-06-30 21:21:38 +0300

    higher level 3

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+# 100-my_calculator.py
 
 if __name__ == "__main__":
-      from calculator_1 import add, sub, mul, div
+    """Handle basic arithmetic operations."""
+    from calculator_1 import add, sub, mul, div
     import sys
 
     if len(sys.argv) - 1 != 3:
