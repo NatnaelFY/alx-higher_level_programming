a5ca8afe610ca8add3a352804e07b5810dd208da
NatnaelFY <natfiky2020@gmail.com>
2022-07-03 20:19:27 +0300

    higher level 4

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,9 @@
+able File  7 lines (7 sloc)  172 Bytes
+
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list
+    else:
+        del my_list[idx]
+    return my_list
