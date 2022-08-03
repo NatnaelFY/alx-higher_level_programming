e40344ca332c006903ca78954271ef33e23e2b69
NatnaelFY <natfiky2020@gmail.com>
2022-08-03 23:29:23 +0300

    higher level 11

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
-"""
-    Filename: 7-add_item.py
-"""
+"""A script that stores arguments to a JSON file."""
 import sys
-import json
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+import os
+from importlib import import_module as using
+
+
+save_to_json_file, load_from_json_file = (
+    using('5-save_to_json_file').save_to_json_file,
+    using('6-load_from_json_file').load_from_json_file
+)
+args_list = []
+file_name = 'add_item.json'
+
+
+def makeScript():
+    if not os.path.exists(file_name):
+        with open(file_name, mode='w', encoding='utf-8') as file:
+            file.write('[]')
+    json_list = load_from_json_file(file_name)
+    if (type(json_list) is list) and all(type(s) is str for s in json_list):
+        args_list.extend(json_list)
+    args_list.extend(sys.argv[1:])
+    save_to_json_file(args_list, file_name)
 
 
 if __name__ == '__main__':
-    f = 'add_item.json'
-    try:
-        cnt = load_from_json_file(f)
-    except FileNotFoundError:
-        cnt = []
-    cnt.extend(sys.argv[1:])
-    save_to_json_file(cnt, f)
-    
+    makeScript()
