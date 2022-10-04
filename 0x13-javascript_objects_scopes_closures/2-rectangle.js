4fbfd341309b7f5b0fb96273d1cdb58e8422c6c6
NatnaelFY <natfiky2020@gmail.com>
2022-10-05 01:49:02 +0300

    Java Script 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 
+// A class that defines a rectangle
+// and checks if w and h are non-zero and
+// non-negative. If not, define an empty object
+
 class Rectangle {
   constructor (w, h) {
     if ((w > 0) && (h > 0)) {
@@ -8,5 +12,4 @@ class Rectangle {
     }
   }
 }
-  
-  module.exports = Rectangle;
+module.exports = Rectangle;
