b71b91912e6c9b0487b372cdd2b4e8384d3b3082
NatnaelFY <natfiky2020@gmail.com>
2022-10-05 01:57:26 +0300

    Java Script 2

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -6,22 +6,22 @@
 // the method print.
 
 class Rectangle {
-    constructor (w, h) {
-      if ((w > 0) && (h > 0)) {
-        this.width = w;
-        this.height = h;
-      }
-    }
-  
-    print () {
-      for (let i = 0; i < this.height; i++) {
-        let s = '';
-        for (let j = 0; j < this.width; j++) {
-          s += 'X';
-        }
-        console.log(s);
-      }
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
     }
   }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += 'X';
+      }
+      console.log(s);
+    }
+  }
+}
   
 module.exports = Rectangle;
